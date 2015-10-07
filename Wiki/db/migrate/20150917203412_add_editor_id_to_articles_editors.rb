4c46c17bea72143ec7808f5c7e9f050295ffccca
JohnNortz <jnnortz@gmail.com>
2015-10-07 14:23:13 -0700

    fixing broken migrates

--- Wiki/db/migrate/20150917203412_add_editor_id_to_articles_editors.rb
+++ Wiki/db/migrate/20150917203412_add_editor_id_to_articles_editors.rb
@@ -1,9 +1,9 @@
 class AddEditorIdToArticlesEditors < ActiveRecord::Migration
   def change
-
-    add_column :articles_editors, :article_id, :integer, index: true
-    add_column :articles_editors, :editor_id, :integer, index: true
     remove_column :articles_editors, :editors_id, :integer
     remove_column :articles_editors, :articles_id, :integer
+    add_column :articles_editors, :article_id, :integer, index: true
+    add_column :articles_editors, :editor_id, :integer, index: true
+
   end
 end
